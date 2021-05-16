47aed0b4d75eaef6ef450dceca5e55693af81be2
ldelsignore <ldelsignore@fordham.edu>
2021-05-16 17:15:07 -0400

    Add transliterations for primary monographs

--- g2p.py
+++ g2p.py
@@ -3,14 +3,81 @@
 from pynini import accep, union, cdrewrite, cross, string_map
 from pynini.lib import rewrite
 
-graphemes = ...
-graphemes = graphemes.optimize()
+monographs = union("あ","い","う","え","お",
+				   "か","き","く","け","こ",
+				   "さ","し","す","せ","そ",
+				   "た","ち","つ","て","と",
+				   "な","に","ぬ","ね","の",
+				   "は","ひ","ふ","へ","ほ",
+				   "ま","み","む","め","も",
+				   "や",     "ゆ",     "よ",
+				   "ら","り","る","れ","ろ",
+				   "ら","り","る","れ","ろ",
+				   "わ",               "を",
+				   "ん")
 
-phonemes = ...
+graphemes = monographs.optimize()
 
-SIGMA_STAR = ...
 
-G2P = ... 
+monograph_phonemes = union("a",	"i", "ɯ", "e", "o",
+				   		   "k", "s", "ɕ", "t", "n",
+						   "ɲ", "h", "ç", "ɸ", "m",
+				   		   "j", "ɾ", "ɰ", "ɴ", "ŋ",
+						   "ɰ̃")
+
+phonemes = monograph_phonemes.optimize()
+
+SIGMA_STAR = union(graphemes, phonemes).closure().optimize()
+
+G2P = (
+    cdrewrite(cross("は", "ɰa"), "[BOS]", "[EOS]", SIGMA_STAR)
+    @ cdrewrite(cross("あ", "a"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("い", "i"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("う", "ɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("え", "e"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("お", "o"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("か", "ka"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("き", "ki"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("く", "kɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("け", "ke"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("こ", "ko"), "", "", SIGMA_STAR)
+	@ cdrewrite(cross("さ", "sa"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("し", "ɕi"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("す", "sɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("せ", "se"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("そ", "so"), "", "", SIGMA_STAR)
+	@ cdrewrite(cross("た", "ta"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("ち", "tɕi"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("つ", "tsɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("て", "te"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("と", "to"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("な", "na"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("に", "ɲi"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("ぬ", "nɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("ね", "ne"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("の", "no"), "", "", SIGMA_STAR)
+	@ cdrewrite(cross("は", "ha"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("ひ", "çi"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("ふ", "ɸɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("へ", "he"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("ほ", "ho"), "", "", SIGMA_STAR)
+	@ cdrewrite(cross("ま", "ma"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("み", "mi"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("む", "mɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("め", "me"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("も", "mo"), "", "", SIGMA_STAR)
+	@ cdrewrite(cross("や", "ja"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("ゆ", "jɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("よ", "jo"), "", "", SIGMA_STAR)
+	@ cdrewrite(cross("ら", "ɾa"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("り", "ɾi"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("る", "ɾɯ"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("れ", "ɾe"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("ろ", "ɾo"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("わ", "ɰa"), "", "", SIGMA_STAR)
+    @ cdrewrite(cross("を", "o"), "", "", SIGMA_STAR)
+)
+
 
 def g2p(istring: str) -> str:
     """Applies the G2P rule.
--- g2p_test.py
+++ g2p_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """Unit tests for Hiragana G2P."""
 
 import unittest
@@ -5,19 +6,55 @@ import g2p
 
 
 class G2PTest(unittest.TestCase):
-    def rewrites(self, istring: str, expected_ostring: str) -> None:
-        """Asserts that the g2p rule produces the correct output.
+	def rewrites(self, istring: str, expected_ostring: str) -> None:
+		"""Asserts that the g2p rule produces the correct output.
 
-        Note that this itself is not a unit test because its name does not
-        begin with `test_`; but it can be used to implement other unit tests.
+		Note that this itself is not a unit test because its name does not
+		begin with `test_`; but it can be used to implement other unit tests.
 
-        Args:
-            istring: the input string
-            expected_ostring: the expected output string.
-        """
-        ostring = g2p.g2p(istring)
-        self.assertEqual(ostring, expected_ostring)
+		Args:
+			istring: the input string
+			expected_ostring: the expected output string.
+		"""
+		ostring = g2p.g2p(istring)
+		self.assertEqual(ostring, expected_ostring)
+
+	def test_ue(self) -> None:
+		self.rewrites("うえ", "ɯe")
+
+	def test_aku(self) -> None:
+		self.rewrites("あく", "akɯ")
+
+	def test_sushi(self) -> None:
+		self.rewrites("すし", "sɯɕi")
+	
+	def test_tsuchi(self) -> None:
+		self.rewrites("つち", "tsɯtɕi")
+
+	def test_tanuki(self) -> None:
+		self.rewrites("たぬき", "tanɯki")
+
+	def test_hoshi(self) -> None:
+		self.rewrites("ほし", "hoɕi")
+
+	def test_hanami(self) -> None:
+		self.rewrites("はなみ", "hanami")
+	
+	def test_yamato(self) -> None:
+		self.rewrites("やまと", "jamato")
+
+	def test_kore(self) -> None:
+		self.rewrites("これ", "koɾe")
+
+	def test_o(self) -> None:
+		self.rewrites("を", "o")
+
+	def test_wa(self) -> None:
+		self.rewrites("は", "ɰa")
+	
+	
+
+
 
 if __name__ == "__main__":
     unittest.main()
-
